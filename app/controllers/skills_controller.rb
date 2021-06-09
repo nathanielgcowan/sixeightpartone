bd46172455bea0b14d75c1dd0ec0330a23826804
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-09 00:15:46 -0500

    finish the skill controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,26 @@
 class SkillsController < ApplicationController
+    def index
+        skills = Skills.all
+        render json: skills
+    end
+
+    def create
+        skill = Skill.new(skill_params)
+        if skill.save
+            render json: skill
+        else
+            rendr json: {erros: Skill.errors.full_messages}
+        end
+    end
+
+    def destroy
+        skill = Skill.find(params[:id])
+        skill.destroy
+    end
+    
+    private
+
+    def skill_params
+        params.require(:skill).permit(:name, :creature_id)
+    end
 end
